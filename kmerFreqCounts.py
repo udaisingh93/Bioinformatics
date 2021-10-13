d9051b76a9df82437df29ed5baddf5178184f887
udai singh <udai.singh93@gmail.com>
2021-10-13 14:30:51 +0200

    Update kmerFreqCounts.py

--- kmerFreqCounts.py
+++ kmerFreqCounts.py
@@ -1,4 +1,3 @@
-# Copy your updated FrequentWords function (along with all required subroutines) below this line
 def FrequentWords(Text, k):
     words = []
     freq = FrequencyMap(Text, k)
@@ -7,8 +6,6 @@ def FrequentWords(Text, k):
         if(freq[key]==m):
             words.append(key)
     return words
-
-# Copy your FrequencyMap() function here.
 def FrequencyMap(Text, k):
     freq = {}
     n = len(Text)
@@ -20,9 +17,7 @@ def FrequencyMap(Text, k):
             freq[Pattern] = 1;
     return freq
 
-# Now set Text equal to the Vibrio cholerae oriC and k equal to 10
 oriC="ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC"
 k=10
 
-# Finally, print the result of calling FrequentWords on Text and k.
 print(FrequentWords(oriC, k))
