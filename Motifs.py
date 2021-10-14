0b1c8501298048fd41e4dae5ce37eadf1454ffe3
udai singh <udai.singh93@gmail.com>
2021-10-14 21:58:03 +0200

    Update Motifs.py

--- Motifs.py
+++ Motifs.py
@@ -11,3 +11,13 @@ def Count(Motifs):
             symbol = Motifs[i][j]
             count[symbol][j] += 1
     return count
+def Profile(Motifs):
+    t = len(Motifs)
+    k = len(Motifs[0])
+    profile = {}
+    profile=Count(Motifs)
+    for i in "ACGT":
+        for j in range(k):
+            profile[i][j]=profile[i][j]/t   
+    # insert your code here
+    return profile
