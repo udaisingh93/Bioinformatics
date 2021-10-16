f3c6dfca3984433767a06cc2ccbe16344660b47a
udai singh <udai.singh93@gmail.com>
2021-10-17 00:38:39 +0200

    Update Motifs.py

--- Motifs.py
+++ Motifs.py
@@ -54,3 +54,16 @@ def Pr(Text, Profile):
     for i in range(len(Text)):
         p*=Profile[Text[i]][i]
     return p
+def CountWithPseudocounts(Motifs):
+    t = len(Motifs)
+    k = len(Motifs[0])
+    count = {} # initializing the count dictionary
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+             count[symbol].append(1)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
